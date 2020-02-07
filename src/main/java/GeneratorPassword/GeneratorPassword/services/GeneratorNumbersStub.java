e7bcf3ae250c28bc2372a86e862465701aa2e4a3
HILDEBOG\lanosce <lanosce@globalhitss.com>
2020-02-07 14:05:18 -0500

    The problem founded when the algorit operate with base 16, repair and see the good number

--- src/main/java/GeneratorPassword/GeneratorPassword/services/GeneratorNumbersStub.java
+++ src/main/java/GeneratorPassword/GeneratorPassword/services/GeneratorNumbersStub.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import java.math.BigInteger;
+import java.util.Collection;
+
 /**
  *
  * @author laboratorio
@@ -17,8 +19,10 @@ import java.math.BigInteger;
 public class GeneratorNumbersStub implements GeneratorNumbersServices {
 
     public List<BigInteger> numeros = new ArrayList<BigInteger>();
-    public GeneratorNumbersStub(){}
-    
+
+    public GeneratorNumbersStub() {
+    }
+
     @Override
     public void gellAllNum(String numero) {
         int num = 0;
@@ -31,79 +35,56 @@ public class GeneratorNumbersStub implements GeneratorNumbersServices {
             num++;
         }
     }
+
     @Override
     public List<BigInteger> getNumeros() {
         return numeros;
     }
-    
+
     @Override
     public List<String> separetaNumbers() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override    
-    public BigInteger conversNumbers(BigInteger numero, Integer base){
-       
-                // Your code here!
-        
-        //BigInteger numero=new BigInteger("1000000000000");
-        
-        
-        
+    @Override
+    public BigInteger conversNumbers(BigInteger numero, Integer base) {
+        List<String> prueba1 = new ArrayList<>();
 
-        //long num=numero;
-        //int bas=base;
-        BigInteger mod=BigInteger.ONE;
-        BigInteger cant=BigInteger.ONE;
-        
-        BigInteger t=BigInteger.ONE;
-        
-        BigInteger resp=BigInteger.ZERO;
-        System.out.println("como va resp in"+resp);
-        
-        
-        while(numero.compareTo(BigInteger.valueOf(base))>=0){
-            System.out.println("Base : "+base);
-            System.out.println("que numero se va trabajando: "+numero);
-            mod=numero.mod(BigInteger.valueOf(base));
-            
-            System.out.println("que arroja el mod: "+mod);
-            System.out.println("que arroja el el div: "+numero.divide(BigInteger.valueOf(base)));
-            numero=numero.divide(BigInteger.valueOf(base));            
+        String prueba = "";
+        BigInteger mod = BigInteger.ONE;
+        BigInteger cant = BigInteger.ONE;
+        BigInteger t = BigInteger.ONE;
+        BigInteger resp = BigInteger.ZERO;
+        //System.out.println("como va resp in"+resp);
 
-            
-            t=t.multiply(mod);
-            t=t.multiply(cant);
-            System.out.println("como va t :"+t);
-            System.out.println("que es resp antes de sumar  :"+resp);
-            
-            //resp=resp.add(resp);
-            resp=resp.add(t);
-            t=BigInteger.ONE;
-            
-            //
+        while (numero.compareTo(BigInteger.valueOf(base)) >= 0) {//System.out.println("Base : "+base);//System.out.println("que numero se va trabajando: "+numero);
+            mod = numero.mod(BigInteger.valueOf(base));//System.out.println("que arroja el mod: "+mod);  //System.out.println("que arroja el el div: "+numero.divide(BigInteger.valueOf(base)));
 
-            //n=n.add(BigInteger.valueOf(resp+mod*cant));
-            cant=cant.multiply(BigInteger.valueOf(10));
-            
-            System.out.println("va quedando : "+resp);
+            numero = numero.divide(BigInteger.valueOf(base));
+            t = t.multiply(mod);
+            //prueba=prueba.concat(t.toString());
+            System.out.println("que se adiciona : "+t.toString());
+            prueba1.add(t.toString());
+            t = t.multiply(cant);//System.out.println("como va t :"+t);//System.out.println("que es resp antes de sumar  :"+resp);
+            resp = resp.add(t);
+
+            t = BigInteger.ONE;
+            cant = cant.multiply(BigInteger.valueOf(10));  //System.out.println("va quedando : "+resp);
         }
-        t=BigInteger.ONE;
-        //System.out.println("miresmos resp: "+resp+" - que es num: "+num+"- que es cant: "+cant);
-        //long m=mod*cant;
-        System.out.println("que es cant : "+cant);
-        t=t.multiply(numero);
-        t=t.multiply(cant);
-        System.out.println("que resp antes : "+resp);
-        
-        //resp=resp.add(resp);
-        resp=resp.add(t);
-        
-        
-        
-        System.out.println("XXXXXXXX"+resp);
-        
-        
+        t = BigInteger.ONE;//System.out.println("que es cant : "+cant);
+        //prueba=prueba.concat(numero.toString());
+        //prueba1.add(numero.toString());
+  
+        for (int i = prueba1.size()-1; i >=0; i--) {
+            String k=prueba1.get(i);
+            prueba=prueba.concat(k);
+        }
+
+        t = t.multiply(numero);
+        t = t.multiply(cant);
+        System.out.println("que resp antes : " + resp);
+        System.out.println("que numero antes : " + prueba);
+        resp = resp.add(t);
         return resp;
     }
 }
