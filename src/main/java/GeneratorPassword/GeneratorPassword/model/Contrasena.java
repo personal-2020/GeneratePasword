9a56f9ab6fbdcdcac717f0a06e5acc8aa62c1859
cesar <cesareduardo824@gmail.com>
2020-07-27 03:11:44 -0500

    Agregando Persistencia

---
+++ src/main/java/GeneratorPassword/GeneratorPassword/model/Contrasena.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GeneratorPassword.GeneratorPassword.model;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import static java.util.Objects.hash;
+import org.springframework.data.annotation.Id;
+
+/**
+ *
+ * @author DicoAdmin
+ */
+public class Contrasena {
+  
+    @Id
+    private String contrasena;
+    
+
+    public Contrasena() {
+    }
+    public Contrasena( String contrasena) throws NoSuchAlgorithmException {
+        this.contrasena = Hash(contrasena);
+        
+    }
+    public String getHash() {
+        return contrasena;
+    }
+
+    public void setHash(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+//    public String getContrasena() {
+//        return contrasena;
+//    }
+//
+//    public void setContrasena(String contrasena) {
+//        this.contrasena = contrasena;
+//    }
+
+
+    public String Hash(String contrasena) throws NoSuchAlgorithmException{
+        byte[] bytesDelMensaje = contrasena.getBytes();
+        MessageDigest resumenDelMensaje = MessageDigest.getInstance("MD5");
+        byte[] bytesDelResumen = resumenDelMensaje.digest(bytesDelMensaje);
+        BigInteger resumenNumero = new BigInteger(1, bytesDelResumen);
+        String resumen = resumenNumero.toString(16);
+        return resumen;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return String.format(
+                "Contrasena[contrasena=%s]",
+                contrasena);
+    }
+
+}
