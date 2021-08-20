0a59145395df96e83d8acc069950dccb60ab25fe
cesar <cesareduardo824@gmail.com>
2021-08-20 02:16:36 -0500

    ARREGLANDO PROBLEMA CORS3

--- src/main/java/GeneratorPassword/GeneratorPassword/GeneratorPasswordApplication.java
+++ src/main/java/GeneratorPassword/GeneratorPassword/GeneratorPasswordApplication.java
@@ -3,7 +3,10 @@ package GeneratorPassword.GeneratorPassword;
 import GeneratorPassword.GeneratorPassword.persistence.MongoDB;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackageClasses = MongoDB.class)
@@ -12,5 +15,15 @@ public class GeneratorPasswordApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GeneratorPasswordApplication.class, args);
 	}
+        
+       	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080");
+			}
+		};
+	}        
 
 }
